53b1e0e6362b0b4b9350eb94896f4bf95dd5466a
soyNelsonValenzuela <nelsonvalenzuelagomez@gmail.com>
2025-01-16 23:19:54 -0300

    refactor: migrate user controller to be use with express

--- app/controller/user.controller.js
+++ app/controller/user.controller.js
@@ -2,25 +2,46 @@ import { Sequelize } from "sequelize";
 import User from "../models/user.model.js";
 import Bootcamp from "../models/bootcamp.model.js";
 import db_bootcamp from "../config/db.Config.js";
-export const createUser = async (user) => {
+import bcrypt from 'bcryptjs';
+
+
+export const createUser = async (req,res) => {
     const transaction = await db_bootcamp.transaction();
     try {
-        const newUser = await User.create({
-            firstName: user.firstName,
-            lastName: user.lastName,
-            email: user.email
-        },
-            { transaction }
-        );
-        await transaction.commit()
-        return newUser;
+        const{firstName,lastName,email}=req.body;
+        const password = await bcrypt.hash(req.body.password, 10);
+        const usuarioExiste = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+        if (!firstName || !lastName || !email || !password) {
+            await transaction.rollback();
+            return res.status(404).json({message: 'Los campos no pueden estar vacios'})
+        } else if (usuarioExiste) {
+            await transaction.rollback();
+            return res.status(400).json({message: 'El usuario ya está registrado'})
+        } else {
+            const usuario = await User.create({
+                firstName,
+                lastName,
+                email,
+                password
+            })
+            await transaction.rollback();
+            return res.status(201).json({
+                message: 'Usuario registrado de forma correcta!!!',
+            ...usuario.dataValues
+        })
+        }
     } catch (error) {
-        console.error(error.stack);
         await transaction.rollback();
+        return res.status(500).json({message: `Ha ocurrido un error ${error.stack}`})
     }
 };
-export const findUserById = async (userData) => {
-    const {id}=userData;
+
+export const findUserById = async (req, res) => {
+    const { id } = req.params; // Obtener el ID de los parámetros de la ruta
     try {
         const usuario = await User.findByPk(id, {
             include: {
@@ -28,20 +49,14 @@ export const findUserById = async (userData) => {
                 attributes: ['id', 'title', 'cue', 'description'], // Campos del usuario a incluir
                 through: { attributes: [] }, // Excluir datos de la tabla intermedia
             },
-            attributes: ['id', 'firstName', 'lastName', 'email', 'createdAt', 'updatedAt'], // Campos del bootcamp a incluir
+            attributes: ['id', 'firstName', 'lastName', 'email','password', 'createdAt', 'updatedAt'], // Campos del bootcamp a incluir
             logging: false,
         });
-
         if (!usuario) {
-            throw new Error(`No se encontró el usuario con ID ${id}`);
+            return res.status(404).json({ message: `No se encontró el usuario con ID ${id}` });
         }
         const result = {
-            id: usuario.id,
-            firstName: usuario.firstName,
-            lastName: usuario.lastName,
-            email: usuario.email,
-            createdAt: usuario.createdAt,
-            updateAt: usuario.updatedAt,
+            ...usuario.dataValues,
             bootcamps: usuario.bootcamps.map(bootcamp => ({
                 id: bootcamp.id,
                 title: bootcamp.title,
@@ -50,13 +65,13 @@ export const findUserById = async (userData) => {
         };
         console.log(`*********************************`);
         console.log(JSON.stringify(result, null, 2));
-        return JSON.stringify(result, null, 2);
+        return res.status(200).json(result); // Responder con JSON al cliente
     } catch (error) {
         console.error(`Error al obtener la información: ${error.message}`);
-        return { message: `Error al obtener la información: ${error.message}` };
+        return res.status(500).json({ message: `Error al obtener la información: ${error.message}` });
     }
 };
-export const findAllUsers = async () => {
+export const findAllUsers = async (req, res) => {
     try {
         const usuarios = await User.findAll({
             include: {
@@ -64,11 +79,11 @@ export const findAllUsers = async () => {
                 attributes: ['id', 'title', 'cue', 'description'],
                 through: { attributes: [] },
             },
-            attributes: ['id', 'firstName', 'lastName', 'email', 'createdAt', 'updatedAt'],
+            attributes: ['id', 'firstName', 'lastName', 'email', 'password', 'createdAt', 'updatedAt'],
             logging: false,
         });
         if (usuarios.length === 0) {
-            throw new Error("No se encontraron usuarios en la base de datos");
+            return res.status(404).json({ message: "No se encontraron usuarios en la base de datos" })
         }
         const result = usuarios.map(user => {
             const bootcamps = user.bootcamps.map(bootcamp => ({
@@ -77,59 +92,65 @@ export const findAllUsers = async () => {
                 cue: bootcamp.cue
             }));
             return {
-                id: user.id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email,
-                createdAt: user.createdAt,
-                updatedAt: user.updatedAt,
+               ...user.dataValues,
                 ...(bootcamps.length > 0 && { bootcamps })
             };
         });
         console.log(JSON.stringify(result, null, 2));
-        return JSON.stringify(result, null, 2);
+        return res.status(200).json(result); // Responder con JSON al cliente
     } catch (error) {
         console.error(`Error al obtener información: ${error.message}`);
         return { message: `Error al obtener información: ${error.message}` };
     }
 };
-export const updateUserById = async (userdata) => {
-    const { id, firstName, lastName, email } = userdata
+export const updateUserById = async (req, res) => {
+    const { firstName, lastName, email } = req.body;
+    const password = await bcrypt.hash(req.body.password,10);
+    const { id } = req.params;
     const transaction = await db_bootcamp.transaction({ logging: false });
     try {
         const user = await User.findByPk(id, { logging: false, transaction })
         if (user) {
-            const userToUpdate = await User.update({ firstName, lastName, email }, { where: { id: id }, returning: true, logging: false, transaction })
+            const userToUpdate = await User.update({ firstName, lastName, email,password }, { where: { id: id }, returning: true, logging: false, transaction })
             console.log(`Usuario id: ${id} actualizado exitosamente.`);
             console.log(JSON.stringify(userToUpdate[1][0].dataValues, null, 2));
             await transaction.commit();
-            return (JSON.stringify(userToUpdate[1][0].dataValues, null, 2))
+            return res.status(200).json({
+                message: `Usuario id: ${id} actualizazdo exitosamente`,
+                ... userToUpdate[1][0].dataValues
+            })
         } else {
-            throw new Error(`El usuario con id: ${id} no existe`);
+            await transaction.rollback();
+            res.status(404).json({message:`El usuario con id: ${id} no existe`});
         }
     } catch (error) {
         console.error(`Error al actualizar información: ${error.message}`);
         await transaction.rollback();
-        return { message: `Error al actualizar información: ${error.message}` };
+        res.status(500).json ({ message: `Error al actualizar información: ${error.message}` });
     };
 };
-export const deleteUserById = async (userData) => {
-    const { id } = userData;
+export const deleteUserById = async (req, res) => {
+    const { id } = req.params;
     const transaction = await db_bootcamp.transaction({ logging: false });
     try {
         const user = await User.findByPk(id, { logging: false, transaction });
         if (user) {
+            const userData = user.dataValues; // Guarda los datos del usuario antes de eliminarlo
             await user.destroy({ logging: false });
             console.log(`Usuario id: ${id} eliminado exitosamente.`);
-            console.log(JSON.stringify(user.dataValues,null,2));
+            console.log(JSON.stringify(userData, null, 2));
             await transaction.commit();
-            return (JSON.stringify(user.dataValues,null,2));
+            return res.status(200).json({
+                message: `Usuario con id: ${id} eliminado exitosamente.`,
+                ...userData
+            });
         } else {
-            throw new Error(`El usuario con id: ${id} no existe`);
+            await transaction.rollback();
+            return res.status(404).json({ message: `El usuario con id: ${id} no existe` });
         }
     } catch (error) {
         console.error(`Error al actualizar información: ${error.message}`);
         await transaction.rollback();
-        return { message: `Error al actualizar información: ${error.message}` };
+        return res.status(500).json({ message: `Error al actualizar información: ${error.message}` });
     }
 };
