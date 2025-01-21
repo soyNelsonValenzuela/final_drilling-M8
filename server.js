38cd49063ad139c80cce75c4243c393b201d689b
soyNelsonValenzuela <nelsonvalenzuelagomez@gmail.com>
2025-01-21 10:50:16 -0300

    feat:create server

--- server.js
+++ server.js
@@ -2,34 +2,25 @@ import express from 'express';
 import cors from 'cors';
 import { config } from 'dotenv';
 import morgan from 'morgan';
-// Importar los módulos propios
-import userRoutes from './app/routes/user.routes.js';
-
+import cookieParser from 'cookie-parser';
+import userRoutes from './src/routes/user.routes.js';
+import bootcampRoutes from './src/routes/bootCamp.routes.js';
 config();
-
 const app = express();
 const { SERVERPORT } = process.env;
-
-// Middlewares
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-// Llamado a las rutas
+app.use(cookieParser());
 app.use(userRoutes);
-
-// Levantar el servidor e inicializar datos
+app.use(bootcampRoutes);
 (async function startServer() {
   try {
     app.listen(SERVERPORT, () => {
       console.log(`Servidor escuchando en http://localhost:${SERVERPORT}`);
     });
-
-    //console.log("Inicializando la base de datos...");
-    //await initializeDatabase(); // Llamar a la inicialización de la base de datos
-    //console.log("Base de datos inicializada correctamente.");
   } catch (error) {
-    //console.error("Error al iniciar el servidor o inicializar la base de datos:", error);
+    console.error("Error al iniciar el servidor o inicializar la base de datos:", error);
   }
 })();
